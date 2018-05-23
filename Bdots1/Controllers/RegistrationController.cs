da12d84941e4e5155e5134f96d37b47892f416ab
Josip Zelić <jzelic00@fesb.hr>
2018-05-23 11:38:49 +0200

    Provjera postoji li isti mail ili username

--- Bdots1/Controllers/RegistrationController.cs
+++ Bdots1/Controllers/RegistrationController.cs
@@ -21,15 +21,32 @@ namespace Bdots1.Controllers
 
         public JsonResult SaveUser(CertUser newCertUser)
         {
+            int state;
             BDEntities connection = new BDEntities();
+            // string poruka = "amen chao";
+            List<CertUser> ListOfUsers = connection.CertUsers.ToList();
+
+            foreach (CertUser d in ListOfUsers)
+            {
+                if (newCertUser.email == d.email)
+                {
+                    state = 2;
+                    return Json(state, JsonRequestBehavior.AllowGet);
+                }
+                else if (newCertUser.username == d.username)
+                {
+                    state = 1;
+                    return Json(state, JsonRequestBehavior.AllowGet);
+                }
+               
+            }
+
+            newCertUser.balance = 0;
             connection.CertUsers.Add(newCertUser);
             connection.SaveChanges();
 
-          //List<CertUser> ListOfUsers = connection.CertUsers.ToList();
-
-            return Json(newCertUser, JsonRequestBehavior.AllowGet);
-
-           
+            state = 0;
+            return Json(state, JsonRequestBehavior.AllowGet);
         }
 
        
--- Bdots1/Scripts/RegistrationScript.js
+++ Bdots1/Scripts/RegistrationScript.js
@@ -5,6 +5,7 @@ myApp.controller('registrationCtrl', function ($scope, $http,$window) {
 
 
 	$scope.certuser = {};
+	var status = {};
 
 	$scope.savedata = function (certuser) {
 		
@@ -12,12 +13,22 @@ myApp.controller('registrationCtrl', function ($scope, $http,$window) {
 		$http.post("/Registration/SaveUser", { newCertUser: certuser })
 			.then(function succesCallback(response) {
 				debugger;
-				$scope.certuser = response.data;
-				alert("Registration successfull");
-				$window.location.href = '/NavigationBar/Index';
+				status = response.data;
+				if (status === 0) {
+					alert("Registration successfull");
+					$window.location.href = '/NavigationBar/Index';
+				}
+				else if(status==1){
+					alert("Username is already in use");
+				}
+				else if (status == 2) {
+					alert("Email is already in use");
+				}
+				
+				
 
 			}, function errorCallback(response) {
-                alert("Invalid input!");
+                alert("POST method error");
 			});
 
 	}
